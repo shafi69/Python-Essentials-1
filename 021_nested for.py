6ab095610b4a57b9e28cea8a0e55bb33497f3a65
shafi69 <shafivirdi67@gmail.com>
2021-12-24 16:18:50 -0500

    assignment

---
+++ 021_nested for.py
@@ -0,0 +1,6 @@
+i=1
+for n in range(1,6):
+    print()
+    print()
+    for a in range(1,6):
+        print(n*a,end="\t")
---
+++ assignment_016_while.py
@@ -0,0 +1,10 @@
+even=-1
+odd=0
+n=1
+while n!=0:
+    n=int(input("Enter the number: "))
+    if n%2==0:
+        even=even+1
+    else: odd=odd+1
+print("The Even count is: ", even)
+print("The Odd count is: ",odd)
